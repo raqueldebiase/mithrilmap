f364531b95c5dd67df2b5f84f94f27fe24d9cf17
Raquel De Biase <raqueldebiase@gmail.com>
2024-06-11 10:45:27 +0100

    add btn reset password firebase

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import styles from './Login.module.css';
-import { Link } from 'react-router-dom';
 import { auth } from '../../../firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { AuthContext } from '../../../AuthContext';
 import CookieConsent from '../../CookiesConsent';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [resetPasswordError, setResetPasswordError] = useState('');
   const navigate = useNavigate();
   const { setCurrentUser } = useContext(AuthContext);
 
@@ -27,6 +28,21 @@ const Login = () => {
     }
   };
 
+  const handleResetPassword = async () => {
+    if (email.trim() === '') {
+      setEmailError('Please enter your email address');
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert('A password reset email has been sent to your email address. Please check your inbox.');
+    } catch (error) {
+      console.error('Error sending password reset email:', error);
+      setResetPasswordError('There was an error sending the password reset email. Please try again later.');
+    }
+  };
+
   return (
     <div className={`${styles.login} modal`}>
       <h2>SHAALL YOOU PAAAS ?</h2>
@@ -35,19 +51,32 @@ const Login = () => {
           className={styles.input}
           type="email"
           name="email"
-          placeholder="Email"
+          placeholder="Email *"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setEmailError('');
+          }}
         />
+        {emailError && <p className={styles.error}>{emailError}</p>}
         <input
           className={styles.input}
           type="password"
           name="password"
-          placeholder="Password"
+          placeholder="Password *"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <button
+          className={styles.resetButton}
+          type="button"
+          onClick={handleResetPassword}
+        >
+          Forgot your password?
+        </button>
         <button className={styles.button} type="submit">Login</button>
+        
+        {resetPasswordError && <p className={styles.error}>{resetPasswordError}</p>}
         <Link className={styles.register} to="/Register">Not register yet? Click here</Link>
       </form>
       <CookieConsent />
