b6887e4b5757783e8ce3572fa9d9718f98b1992d
Raquel De Biase <raqueldebiase@gmail.com>
2024-06-17 18:14:34 +0100

    fix mobile parallax final

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,49 +1,75 @@
 import React, { useEffect } from 'react';
-import  { useLocation } from 'react-router-dom';
-import styles from './Parallax.module.css';
-import svg1 from '../../assets/wave1.svg';
-import svg2 from '../../assets/wave2.svg';
-import svg3 from '../../assets/wave3.svg';
-import svg4 from '../../assets/wave4.svg';
+import styles from './Parallax.module.css'; // Ajuste o caminho conforme necessário
+import svg1 from '../../assets/wave1.svg'; // Ajuste o caminho conforme necessário
+import svg2 from '../../assets/wave2.svg'; // Ajuste o caminho conforme necessário
+import svg3 from '../../assets/wave3.svg'; // Ajuste o caminho conforme necessário
+import svg4 from '../../assets/wave4.svg'; // Ajuste o caminho conforme necessário
 
-const Parallax = () => {
+const ParallaxComponent = ({ isHomeRoute }) => {
+  useEffect(() => {
+    const adjustSpeed = () => {
+      const layers = document.querySelectorAll('[data-speed]');
+      layers.forEach(layer => {
+        if (window.innerWidth < 540) {
+          switch (layer.className) {
+            case `${styles.parallax} ${styles.layer1}`:
+              layer.setAttribute('data-speed', '25'); // Ajuste o valor conforme necessário
+              break;
+            case `${styles.parallax} ${styles.layer2}`:
+              layer.setAttribute('data-speed', '5'); // Ajuste o valor conforme necessário
+              break;
+            case `${styles.parallax} ${styles.layer3}`:
+              layer.setAttribute('data-speed', '10'); // Ajuste o valor conforme necessário
+              break;
+            case `${styles.parallax} ${styles.layer4}`:
+              layer.setAttribute('data-speed', '25'); // Ajuste o valor conforme necessário
+              break;
+            default:
+              break;
+          }
+        } else {
+          switch (layer.className) {
+            case `${styles.parallax} ${styles.layer1}`:
+              layer.setAttribute('data-speed', '200');
+              break;
+            case `${styles.parallax} ${styles.layer2}`:
+              layer.setAttribute('data-speed', '10');
+              break;
+            case `${styles.parallax} ${styles.layer3}`:
+              layer.setAttribute('data-speed', '20');
+              break;
+            case `${styles.parallax} ${styles.layer4}`:
+              layer.setAttribute('data-speed', '100');
+              break;
+            default:
+              break;
+          }
+        }
+      });
+    };
 
-    const location = useLocation();
-    const isHomeRoute = location.pathname === '/home';
+    adjustSpeed();
+    window.addEventListener('resize', adjustSpeed);
 
+    return () => window.removeEventListener('resize', adjustSpeed);
+  }, []);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const parallaxElements = document.querySelectorAll(`.${styles.parallax}`);
-            parallaxElements.forEach(element => {
-                const speed = parseFloat(element.getAttribute('data-speed'));
-                const yPos = -(window.pageYOffset * speed / 100);
-                element.style.transform = `translateY(${yPos}px)`;
-            });
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-        <div className={`${styles.parallaxContainer} ${isHomeRoute ? styles.homeRoute : ''}`}>
-            <div className={`${styles.parallax} ${styles.layer1}`} data-speed="200">
-                <img src={svg1} alt="Layer 1" />
-            </div>
-            <div className={`${styles.parallax} ${styles.layer2}`} data-speed="10">
-                <img src={svg2} alt="Layer 2" />
-            </div>
-            <div className={`${styles.parallax} ${styles.layer3}`} data-speed="20">
-                <img src={svg3} alt="Layer 3" />
-            </div>
-            <div className={`${styles.parallax} ${styles.layer4}`} data-speed="100">
-                <img src={svg4} alt="Layer 4" />
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${styles.parallaxContainer} ${isHomeRoute ? styles.homeRoute : ''}`}>
+      <div className={`${styles.parallax} ${styles.layer1}`} data-speed="200">
+        <img src={svg1} alt="Layer 1" />
+      </div>
+      <div className={`${styles.parallax} ${styles.layer2}`} data-speed="10">
+        <img src={svg2} alt="Layer 2" />
+      </div>
+      <div className={`${styles.parallax} ${styles.layer3}`} data-speed="20">
+        <img src={svg3} alt="Layer 3" />
+      </div>
+      <div className={`${styles.parallax} ${styles.layer4}`} data-speed="100">
+        <img src={svg4} alt="Layer 4" />
+      </div>
+    </div>
+  );
 };
 
-export default Parallax;
+export default ParallaxComponent;
