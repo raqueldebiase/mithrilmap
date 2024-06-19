d83d32722572f82fdc27ec42e0b70bf817df68bf
Raquel De Biase <raqueldebiase@gmail.com>
2024-06-19 18:23:28 +0100

    fix read chapters list do profile page

--- src/components/PageProfile.jsx
+++ src/components/PageProfile.jsx
@@ -1,22 +1,50 @@
-import React, { useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './PageProfile.module.css';
 import { useReadingProgress } from './ReadChapterList'; // Verifique o caminho correto
 import { readChaptersObservable, sendChapters } from './resetProgressObservable';
 
 const PageProfile = () => {
-
   const [readChaptersList, setReadChaptersList] = useState([]);
-
   const { readChapters } = useReadingProgress(); // Obtém os capítulos lidos do contexto
 
+  const chaptersTitles = [
+    { id: 1, title: 'The Silmarillion: Ainulindalë', avatar: '../assets/silmarills.png' },
+    { id: 2, title: 'The Silmarillion: Valaquenta' },
+    { id: 3, title: 'The Silmarillion: Quenta Silmarillion (up to the end of Chapter XVIII)' },
+    { id: 4, title: 'Beren and Lúthien: read the entire book' },
+    { id: 5, title: 'The Silmarillion: read Chapter XX' },
+    { id: 6, title: 'The Children of Húrin: read the entire book OR The Silmarillion: read Chapter XXI' },
+    { id: 7, title: 'Unfinished Tales: read Chapter I of the first part of Unfinished Tales “Of Tuor and his coming to Gondolin”' },
+    { id: 8, title: 'The Silmarillion: continue reading The Silmarillion in the part “Quenta Silmarillion”, chapters XXIII and XXIV' },
+    { id: 9, title: 'Unfinished Tales (Part II): read the chapters: I – A Description of the Island of Númenor, II – Aldarion and Erendis, III – The Line of Elros: Kings of Númenor' },
+    { id: 10, title: 'The Silmarillion: read “Akallabêth” in its entirety' },
+    { id: 11, title: 'Unfinished Tales: read in the second part Chapter IV “The History of Galadriel and Celeborn”' },
+    { id: 12, title: 'The Silmarillion: read “Of the Rings of Power and the Third Age” up to the 30th paragraph.' },
+    { id: 13, title: 'Unfinished Tales: read “The Disaster of the Gladden Fields”.' },
+    { id: 14, title: 'The Lord of the Rings, The Return of the King, Appendix “A”: read the appendices “Annals of the Kings and Rulers”, up to the end of topic IV “Gondor and the heirs of Anárion”.' },
+    { id: 15, title: 'Unfinished Tales: read the chapter “Cirion and Eorl and the Friendship of Gondor and Rohan”' },
+    { id: 16, title: 'The Lord of the Rings, The Return of the King: read the entire second topic “The House of Eorl”.' },
+    { id: 17, title: 'The Lord of the Rings, The Return of the King, in Appendix “A”: read the topic “part of the story of Arwen and Aragorn”.' },
+    { id: 18, title: 'Unfinished Tales: read the fourth part in the following order: “The Istari”, “The Palantíri”, “The Drúedain”,' },
+    { id: 19, title: 'The Lord of the Rings, The Return of the King, in Appendix “A”: read the topic “The People of Durin”.' },
+    { id: 20, title: 'Unfinished Tales, third part: read “The Quest of Erebor”.' },
+    { id: 21, title: 'The Hobbit: read the entire book' },
+    { id: 22, title: 'The Lord of the Rings: The Fellowship of the Ring: read the entire book' },
+    { id: 23, title: 'Unfinished Tales: read the chapter "The Hunt for the Ring".' },
+    { id: 24, title: 'The Lord of the Rings: The Two Towers: read the first chapter.' },
+    { id: 25, title: 'Unfinished Tales: read the chapter “The Battles of the Fords of Isen”.' },
+    { id: 26, title: 'The Lord of the Rings, The Two Towers: read from the second chapter to the end of the book.' },
+    { id: 27, title: 'The Lord of the Rings, The Return of the King: read the entire book.' },
+    { id: 28, title: 'The Silmarillion: read “Of the Rings of Power and the Third Age”, starting from the 31st paragraph.' },
+  ];
 
   useEffect(() => {
     console.log('useEffect foi adicionado');
     const subscription = readChaptersObservable.subscribe(value => {
       console.log('valor recebido:', value);
       if (value) {
-        setReadChaptersList(value)
+        setReadChaptersList(value);
       }
     });
 
@@ -24,7 +52,6 @@ const PageProfile = () => {
       subscription.unsubscribe();
     };
   }, []);
-  
 
   return (
     <div className={`${styles.profileContent} container`}>
@@ -32,13 +59,14 @@ const PageProfile = () => {
         <div className={styles.readChapters}>
           <h3>Chapters Read:</h3>
           <ul>
-          {readChaptersList.length > 0 ? (
-            readChaptersList.map((chapterId, index) => (
-              <li key={index}>Chapter {chapterId}</li>
-            ))
-          ) : (
-            <li>No chapters read yet.</li>
-          )}
+            {readChaptersList.length > 0 ? (
+              readChaptersList.map((chapterId, index) => {
+                const chapter = chaptersTitles.find(ch => ch.id === chapterId);
+                return <li key={index}>Chapter {chapter ? chapter.title : 'Unknown'}</li>;
+              })
+            ) : (
+              <li>No chapters read yet.</li>
+            )}
           </ul>
         </div>
       </div>
