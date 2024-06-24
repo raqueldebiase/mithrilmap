4a6d509e00ec88d6937b0ef9f8f141646e12369d
Raquel De Biase <raqueldebiase@gmail.com>
2024-06-24 12:03:08 +0100

    add github to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import reactIcon from '../assets/react-svgrepo-com.svg';
+import githubIcon from '../assets/github-outline.svg';
 
 const Footer = () => {
 
@@ -9,14 +10,16 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.copy}>
-      <div>
-      <span>&copy; {currentYear} <a href='https://raqueldebiase.netlify.app/'>Raquel De Biase.</a></span>
-    </div>
         <div>
-        <p>Project developed in React. teste<span><img className={styles.reactIcon} src={reactIcon}></img></span></p>
+          <span>&copy; {currentYear} <a href='https://raqueldebiase.netlify.app/'>Raquel De Biase.</a></span>
+        </div>
+        <div>
+          <p>Project developed in React.<span><img className={styles.reactIcon} src={reactIcon}></img></span></p>
         </div> 
       </div>
-
+      <div className={styles.linkGithub}>
+          <p><a href="https://github.com/raqueldebiase/mithrilmap/blob/main/README.md" target='_blank'>See the project on GitHub.<span><img className={styles.reactGithub} src={githubIcon}></img></span></a></p>
+      </div>
     </div>
   )
 }
